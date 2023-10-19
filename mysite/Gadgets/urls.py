082d6d3f69d7556eab16cf8b6c4b33e90a58b6dc
Chinmay <chinmaymayekar06@gmail.com>
2023-10-20 01:33:28 +0530

    commit:7

--- mysite/Gadgets/urls.py
+++ mysite/Gadgets/urls.py
@@ -6,4 +6,6 @@ app_name = 'Gadgets'
 urlpatterns = [
     path('home/',views.index , name='index'),
     path('detail/<int:item_id>/',views.detail , name='detail'),
+    path('add/',views.create_item , name='create_item'),
+    
 ]
--- mysite/Gadgets/views.py
+++ mysite/Gadgets/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from Gadgets.models import Item
+from Gadgets.forms import ItemForm
 
 # Create your views here.
 
@@ -22,3 +23,17 @@ def detail(request , item_id):
     }
 
     return render(request , 'Gadgets/detail.html', context)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('Gadgets:index')
+    
+    context = {
+        'form': form
+    }
+    
+    return render(request , 'Gadgets/item-form.html' , context)
+
